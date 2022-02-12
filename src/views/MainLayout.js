07128a1514b8988a7bbb71cada369e1e17541311
Fatih Kaan Salgır <31157309+fatihkaan22@users.noreply.github.com>
2022-02-12 17:59:26 +0300

    todos are removed

--- src/views/MainLayout.js
+++ src/views/MainLayout.js
@@ -2,19 +2,8 @@ import React from 'react';
 import Frame from '../components/Frame';
 
 import GridLayout from '../components/Grid';
-import { Checkbox, FormControlLabel } from '@mui/material';
 import { MAIN_LAYOUT } from '../constants/plugins';
 
-// TODO: delete
-const todo = (arr) => (
-  <div style={{ marginLeft: 10 }}>
-    {arr.map((e) => (
-      <div>
-        <FormControlLabel control={<Checkbox />} label={e} />
-      </div>
-    ))}
-  </div>
-);
 const MainLayout = () => {
   return (
     <GridLayout editable>
@@ -32,27 +21,6 @@ const MainLayout = () => {
           </div>
         );
       })}
-
-      <div key="todo" data-grid={{ x: 0, y: 3, w: 3, h: 8 }}>
-        <Frame title="TODO">
-          {todo([
-            'Consider new window components',
-            'Consider dnd to toolbox/sidebar',
-            'react-grid: local storage',
-            'zoom out: cancel draggable'
-          ])}
-        </Frame>
-      </div>
-      <div key="todo2" data-grid={{ x: 3, y: 2, w: 3, h: 8 }}>
-        <Frame title="TODO - postponed">
-          {todo([
-            'Rostopic list',
-            'turn on/off fetch ros data',
-            'hoc: withRos (data provider)',
-            'ros components: online/offline status'
-          ])}
-        </Frame>
-      </div>
     </GridLayout>
   );
 };
