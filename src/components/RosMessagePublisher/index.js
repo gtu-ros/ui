fda9b6a2c0b44c2997b8e3ca392bce5420d143c6
fatihkaan22 <fatihkaan22@gmail.com>
2022-02-27 14:11:04 +0300

    fix: goal messge

--- src/components/RosMessagePublisher/index.js
+++ src/components/RosMessagePublisher/index.js
@@ -25,7 +25,7 @@ const RosMessagePublisher = ({}) => {
             frame_id: 'odom'
           },
           pose: {
-            position: message.position,
+            position: message.pose.position,
             orientation: { x: 0, y: 0, z: 0, w: 1 }
           }
         }
