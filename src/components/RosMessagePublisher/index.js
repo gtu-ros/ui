573824a4ab02aa38e441f51a16bef036b0891184
Fatih Kaan Salgır <31157309+fatihkaan22@users.noreply.github.com>
2022-02-27 10:09:07 +0300

    move base message publisher introduced

--- src/components/RosMessagePublisher/index.js
+++ src/components/RosMessagePublisher/index.js
@@ -1,6 +1,11 @@
+import { Send } from '@mui/icons-material';
+import { Button, Fab } from '@mui/material';
+import ReactJson from 'react-json-view';
 import usePublisher from '../../hooks/usePublisher';
+import useSubscribeTopic from '../../hooks/useSubscribeTopic';
 
 const RosMessagePublisher = ({}) => {
+  const { message } = useSubscribeTopic('/output_point', 200);
   const topic = {
     name: '/test',
     type: 'geometry_msgs/PoseStamped',
@@ -8,32 +13,52 @@ const RosMessagePublisher = ({}) => {
   };
 
   const [publish] = usePublisher(topic);
+  console.log('message publisher', { message });
 
-  const message = {
-    header: {
-      frame_id: 'map'
-    },
-    pose: {
-      position: {
-        x: -0.03,
-        y: -0.81,
-        z: -0.0
-      },
-      orientation: {
-        x: 0,
-        y: 0,
-        z: -0.91,
-        w: -0.39
-      }
-    }
-  };
+  // const message = {
+  //   header: {
+  //     frame_id: 'map'
+  //   },
+  //   pose: {
+  //     position: {
+  //       x: -0.03,
+  //       y: -0.81,
+  //       z: -0.0
+  //     },
+  //     orientation: {
+  //       x: 0,
+  //       y: 0,
+  //       z: -0.91,
+  //       w: -0.39
+  //     }
+  //   }
+  // };
 
   const handlePublish = () => {
-    console.log('here');
     publish(message);
   };
 
-  return <button onClick={handlePublish}>publish</button>;
+  return (
+    <>
+      <ReactJson
+        src={message}
+        style={{ paddingLeft: 4 }}
+        name={false}
+        indentWidth={2}
+        displayDataTypes={false}
+        displayObjectSize={false}
+      />
+
+      <Fab
+        style={{ position: 'absolute', right: 10, bottom: 10 }}
+        onClick={handlePublish}
+        size="small"
+        color="secondary"
+      >
+        <Send />
+      </Fab>
+    </>
+  );
 };
 
 export default RosMessagePublisher;
