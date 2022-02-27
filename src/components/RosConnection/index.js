ef3b73dd68ad66aaa7150679d3e7af937326bb82
fatihkaan22 <fatihkaan22@gmail.com>
2022-02-27 13:08:39 +0300

    ros connection url get/set search params

--- src/components/RosConnection/index.js
+++ src/components/RosConnection/index.js
@@ -1,19 +1,25 @@
 import { Switch, TextField } from '@mui/material';
 import { useState } from 'react';
 import { useROS } from 'react-ros';
+import { useSearchParams } from 'react-router-dom';
 
 const RosConnection = () => {
   const { isConnected, url, changeUrl, toggleConnection } = useROS();
   const [isEdit, setIsEdit] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const urlFromSearchParams = searchParams.get('ROS_URL');
+
   const rosUrl =
-    url ||
+    urlFromSearchParams ||
     'ws://' +
       process.env.REACT_APP_ROS_BRIDGE_URL +
       ':' +
       process.env.REACT_APP_ROS_BRIDGE_PORT;
 
   const handleOnBlur = (e) => {
-    changeUrl(e.target.value);
+    const { value } = e.target;
+    changeUrl(value);
+    setSearchParams({ ROS_URL: value });
     setIsEdit(false);
   };
 
@@ -30,7 +36,9 @@ const RosConnection = () => {
           onBlur={handleOnBlur}
         />
       ) : (
-        <span onClick={() => setIsEdit(true)}>{url}</span>
+        <span onClick={() => setIsEdit(true)}>
+          {urlFromSearchParams || url}
+        </span>
       )}
     </div>
   );
