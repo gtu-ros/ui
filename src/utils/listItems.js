3426798dd65ac74e64c92f0e9d94f1f733bf352c
fatihkaan22 <fatihkaan22@gmail.com>
2022-07-17 13:27:26 +0300

    drive controls link

--- src/utils/listItems.js
+++ src/utils/listItems.js
@@ -8,6 +8,7 @@ import GridOnIcon from '@mui/icons-material/GridOn';
 import VideocamIcon from '@mui/icons-material/Videocam';
 import MapIcon from '@mui/icons-material/Map';
 import NavigationIcon from '@mui/icons-material/Navigation';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
 import BiotechIcon from '@mui/icons-material/Biotech';
 import PrecisionManufacturingIcon from '@mui/icons-material/PrecisionManufacturing';
 import { cameraConfig } from './constants';
@@ -15,75 +16,91 @@ import { Collapse, Divider, List } from '@mui/material';
 import { ExpandLess } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 import { styled } from '@mui/material/styles';
+import useRosWs from '../hooks/useRosWs';
 
 const StyledLink = styled(Link)(({ theme }) => ({
   textDecoration: 'none',
   color: theme.palette.grey[900]
 }));
 
-export const MainListItems = (props) => (
-  <div>
-    <ListSubheader inset style={{ textAlign: 'left' }}>
-      Dashboard
-    </ListSubheader>
+export const MainListItems = (props) => {
+  const { url } = useRosWs();
+  return (
+    <div>
+      <ListSubheader inset style={{ textAlign: 'left' }}>
+        Dashboard
+      </ListSubheader>
 
-    <StyledLink to="/">
-      <ListItem button>
+      <StyledLink to="/">
+        <ListItem button>
+          <ListItemIcon>
+            <DashboardIcon />
+          </ListItemIcon>
+          <ListItemText primary="Main" />
+        </ListItem>
+      </StyledLink>
+      <StyledLink to="/arm">
+        <ListItem button>
+          <ListItemIcon>
+            <PrecisionManufacturingIcon />
+          </ListItemIcon>
+          <ListItemText primary="Robotic Arm" />
+        </ListItem>
+      </StyledLink>
+      <StyledLink to="/navigation">
+        <ListItem button>
+          <ListItemIcon>
+            <NavigationIcon />
+          </ListItemIcon>
+          <ListItemText primary="Navigation" />
+        </ListItem>
+      </StyledLink>
+      <StyledLink to="/science">
+        <ListItem button>
+          <ListItemIcon>
+            <BiotechIcon />
+          </ListItemIcon>
+          <ListItemText primary="Science" />
+        </ListItem>
+      </StyledLink>
+
+      <Divider />
+
+      <ListSubheader inset style={{ textAlign: 'left' }}>
+        Views
+      </ListSubheader>
+
+      <ListItem
+        button
+        onClick={() => {
+          window.open(
+            `${window.location.origin}/urdf`,
+            'urdf',
+            'width=640,height=480'
+          );
+        }}
+      >
         <ListItemIcon>
-          <DashboardIcon />
+          <GridOnIcon />
         </ListItemIcon>
-        <ListItemText primary="Main" />
+        <ListItemText primary="URDF" />
       </ListItem>
-    </StyledLink>
-    <StyledLink to="/arm">
-      <ListItem button>
+
+      <ListItem
+        button
+        component={Link}
+        to={`/DRIVE_CONTROLS?ROS_URL=${url}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <ListItemIcon>
-          <PrecisionManufacturingIcon />
+          <SportsEsportsIcon />
         </ListItemIcon>
-        <ListItemText primary="Robotic Arm" />
+        <ListItemText primary="Drive Controls" />
       </ListItem>
-    </StyledLink>
-    <StyledLink to="/navigation">
-      <ListItem button>
-        <ListItemIcon>
-          <NavigationIcon />
-        </ListItemIcon>
-        <ListItemText primary="Navigation" />
-      </ListItem>
-    </StyledLink>
-    <StyledLink to="/science">
-      <ListItem button>
-        <ListItemIcon>
-          <BiotechIcon />
-        </ListItemIcon>
-        <ListItemText primary="Science" />
-      </ListItem>
-    </StyledLink>
-
-    <Divider />
-
-    <ListSubheader inset style={{ textAlign: 'left' }}>
-      Views
-    </ListSubheader>
-
-    <ListItem
-      button
-      onClick={() => {
-        window.open(
-          `${window.location.origin}/urdf`,
-          'urdf',
-          'width=640,height=480'
-        );
-      }}
-    >
-      <ListItemIcon>
-        <GridOnIcon />
-      </ListItemIcon>
-      <ListItemText primary="URDF" />
-    </ListItem>
-  </div>
-);
-
+    </div>
+  );
+};
 
 // for ip cameras
 export const Cameras = () => {
