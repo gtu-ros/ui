fed4c6d4b2b0e61a2109f198902a744e83fc32a0
fatihkaan22 <fatihkaan22@gmail.com>
2022-07-17 18:15:05 +0300

    log rosout

--- src/components/Rosout/index.js
+++ src/components/Rosout/index.js
@@ -4,26 +4,33 @@ import { PLUGIN_KEYS } from '../../constants';
 import usePluginState from '../../hooks/usePluginState';
 import _ from 'lodash';
 import LogMessage from '../LogMessage';
+import useMessage from '../../hooks/useMessage';
+
 const Rosout = () => {
-  const { setOnline, setOffline } = usePluginState(PLUGIN_KEYS.ROSOUT);
-  const [data, setData] = useState([]);
-  const { isConnected, message } = useSubscribeTopic('/rosout', 0);
+  const { setOnline, setOffline, data, setData } = usePluginState(
+    PLUGIN_KEYS.ROSOUT
+  );
+  const { message } = useMessage(PLUGIN_KEYS.ROSOUT, '/rosout', 0);
 
   useEffect(() => {
-    if (isConnected && message) {
+    if (message) {
       setOnline();
-      setData([...data.slice(-300), message]);
+      const { list } = data || { list: [] };
+      setData({
+        list: [...list?.slice(-300), message],
+        timestamp: message?.header?.stamp?.secs
+      });
     } else {
       setOffline();
     }
-  }, [isConnected, message]);
+  }, [message]);
 
   if (!message) return null;
 
   return (
     <div style={{ margin: 8 }}>
       <pre>
-        {data.map((message) => (
+        {data?.list?.map((message) => (
           <LogMessage message={message} />
         ))}
       </pre>
