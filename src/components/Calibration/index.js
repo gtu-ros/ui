83a9e89ee099438da63a3d756c1cb9512457a317
ilkerymz <113921112+ilkerymz@users.noreply.github.com>
2023-03-11 21:23:59 +0300

    Added a rosparam part (#4)

--- src/components/Calibration/index.js
+++ src/components/Calibration/index.js
@@ -3,21 +3,39 @@ import { useForm } from 'react-hook-form';
 import GpsFixedIcon from '@mui/icons-material/GpsFixed';
 import { PLUGIN_KEYS } from '../../constants';
 import usePluginState from '../../hooks/usePluginState';
+import ROSLIB from 'roslib/src/RosLib';
+import { useROS } from 'react-ros';
 
 const Calibration = () => {
   const { register, handleSubmit } = useForm();
   const { data, setData } = usePluginState(PLUGIN_KEYS.CALIBRATION);
+  const { ros } = useROS();
 
   const fields = {
     latitude: 'latitude',
-    longitude: 'longitude'
+    longitude: 'longitude',
+    head: 'head'
   };
 
+  const isHeading = new ROSLIB.Param({
+    ros: ros,
+    name: 'datum'
+  });
+
   const onSubmit = (submitData) => {
+
+    const latitude = parseFloat(submitData[fields.latitude]);
+    const longitude = parseFloat(submitData[fields.longitude]);
+    const head = parseFloat(submitData[fields.head]);
+
     setData({
-      latitude: submitData[fields.latitude],
-      longitude: submitData[fields.longitude]
+      latitude: latitude,
+      longitude: longitude,
+      head: head,
     });
+
+    isHeading.set([latitude, longitude, head]);
+
   };
 
   return (
@@ -38,6 +56,13 @@ const Calibration = () => {
               variant="outlined"
               label="Initial Longitude"
               {...register(fields.longitude)}
+            />
+            <TextField
+              value={data?.head}
+              size="small"
+              variant='outlined'
+              label="Heading"
+              {...register(fields.head)}
             />
           </Box>
           <Button
